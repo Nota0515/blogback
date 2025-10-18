095804f23a62967a7dd0114e8375a8bf042a5a9f
Nota0515 <businessbro78@gmail.com>
2025-10-18 23:18:08 +0530

    setup the pool connection of database

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const 
+
+const pool = new Pool({
+    host: process.env.HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE,
+    password: process.env.DATABASE_PASSWORD,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+    maxLifetimeSeconds: 60
+})
+
+pool.on('connect' , ()=>{
+    console.log("connction sucessful we created a active pool")
+});
+
+pool.on('error' , (e)=>{
+    console.log(`we got this error ${e} while pool creation`)
+});
+
+module.exports = pool;
