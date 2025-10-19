3c821c0ef51c2c4a21b71bf38c4ee2f489de23d7
Nota0515 <businessbro78@gmail.com>
2025-10-19 13:17:19 +0530

    fixed schemas error

--- config/schemas.sql
+++ config/schemas.sql
@@ -1,24 +1,24 @@
 CREATE TABLE admin (
-    id SERIAL ,
+    id SERIAL PRIMARY KEY ,
     email VARCHAR(225) UNIQUE NOT NULL ,
     password_hash VARCHAR(255) NOT NULL ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE blogs (
-    id SERIAL ,
+    id SERIAL PRIMARY KEY ,
     title VARCHAR(225) NOT NULL,
     content TEXT NOT NULL ,
     slug VARCHAR(225) UNIQUE NOT NULL,
     like_count INTEGER DEFAULT 0 ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE blog_likes (
-    id SERIAL , 
+    id SERIAL PRIMARY KEY , 
     blog_id INTEGER REFERENCES blogs(id) ON DELETE CASCADE,
     user_id VARCHAR(225) NOT NULL , --we will use Ip base address here 
     liked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(blog_id , user_id)
-)
+);
