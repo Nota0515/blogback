51488b6b4eb8a0e86b969ec9fbb71d7368170f0c
Nota0515 <businessbro78@gmail.com>
2025-10-22 23:14:00 +0530

    fixed issues

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -10,12 +10,12 @@ const login = async(req , res) => {
             [email]
         );
 
-        if (result.rows.length === 0) return res.status(401).json({message : "Invalid credential"});
+        if (result.rows.length === 0) return res.status(401).json({error : "Invalid credential"});
         const admin = result.rows[0];
         //compare the password
         const validpassword = await bycrpt.compare(password , admin.password);
         if (!validpassword){
-            return res.status(401).json({message : "Invalid credential"});
+            return res.status(401).json({error : "Invalid credential"});
         };
 
         const token = jwt.sign({
@@ -26,10 +26,9 @@ const login = async(req , res) => {
         res.status(200).json({token});
           
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
         res.status(500).json({
-            message : "server error : " ,
-            error : error.message 
+            error : "Internal server error "
         });
     }
 };
