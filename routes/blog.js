b2f9c89d01944d0449bfca05db43287060cbeb65
Nota0515 <businessbro78@gmail.com>
2025-10-23 22:57:21 +0530

    added the like adding route

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { viewblogs , sblog } = require('../controllers/blogcontroller');
+const { viewblogs , sblog , likedblog } = require('../controllers/blogcontroller');
 
 router.get('/' , viewblogs );
-
 router.get('/:slug', sblog );
+router.post('/:slug/like' , likedblog )
 
 module.exports = router ;
