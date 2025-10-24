03b81db12d36ce9260fee0446068846f10d0fda5
Nota0515 <businessbro78@gmail.com>
2025-10-24 22:35:35 +0530

    change the database schemas

--- config/schemas.sql
+++ config/schemas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE admin (
     id SERIAL PRIMARY KEY ,
     email VARCHAR(225) UNIQUE NOT NULL ,
-    password_hash VARCHAR(255) NOT NULL ,
+    password VARCHAR(255) NOT NULL ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
