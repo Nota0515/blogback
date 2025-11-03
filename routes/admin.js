abc7fabe986cb9f2e9952af523de43b5d4d2fa39
Nota0515 <businessbro78@gmail.com>
2025-11-03 11:50:09 +0530

    import bug fixed

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
-const protect = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const { postblog , deleteblog } = require('../controllers/admincontroller.')
 const express = require('express');
 const router = express.Router();
 
-router.post('/blog/v1/new' , protect , postblog );
-router.delete('/blog/:slug' , protect , deleteblog );
+router.post('/blog/v1/new' , auth , postblog );
+router.delete('/blog/:slug' , auth , deleteblog );
 
 module.exports = router;
