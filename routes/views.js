d9eb11f7c697aa2a855b5ab6c8ca4e035135f166
Nota0515 <businessbro78@gmail.com>
2025-11-15 18:27:50 +0530

    created the routes handlers for all the SSR

--- routes/views.js
+++ routes/views.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const Router = express.Router();
+const { blogs , thisblog } = require('../controllers/blogcontroller');
 
 Router.get('/' , (req , res) =>{
     res.render('index');
 });
 
+Router.get("/blog", async (req, res) => {
+  try {
+    const blogdata = await blogs();
+    res.render("blog", { blogdata });
+  } catch (err) {
+    console.error("Error loading blogs:", err);
+    res.status(500).send("Error loading blog list");
+  }
+});
 
+Router.get('/blog/:slug' , async(req , res)=> {
+    try {
+        const slug = req.params.slug ;
+        const sblog = await thisblog(slug);
+
+        if(!sblog){
+            return res.status(404).render('404');
+        };
+
+        res.render('sblog' , {sblog});
+    } catch (error) {
+        console.error(error);
+        console.log("error in the datahelper of specific blog" , error);
+        res.status(500).send("error leading specific blog");
+    }
+});
+
+
+Router.get('/exp' , (req ,res)=>{
+    res.render('workexp');
+});
 
 module.exports = Router;
