93a5b93fc644386aeadb221f902091c98a348af1
Nota0515 <businessbro78@gmail.com>
2025-12-03 20:06:51 +0530

    rewrote the code logics to handle cookie and SSR requests

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,23 +2,31 @@ const pool = require('../config/db');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-exports.auth = async (req , res , next ) => {
+exports.auth = async (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1];
-        console.log(token);
-        if (!token) return res.status(401).json({ message: "Authentication Required , TOKEN NOT FOUND" });
+        const token =
+            req.cookies?.adminToken ||
+            req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            return req.accepts('html')
+                ? res.redirect('/admin/login')
+                : res.status(401).json({ message: "Invalid or Token expired" });
+        };
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log('verified done');
         req.admin = await pool.query("SELECT * FROM admin WHERE id = $1", [decoded.adminId]);
 
         next();
 
     } catch (error) {
-        if (error.name === 'JsonWebTokenError'){
-            return res.status(401).json({message : 'Invalid Token'});
+        if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+            return req.accepts('html')
+                ? res.redirect('/admin/login')
+                : res.status(401).json({ message: 'Invalid Token' });
         }
-        if (error.name === 'TokenExpiredError'){
-           return res.status(401).json({message : 'Token Expired'});
-        };
-        return res.status(500).json({message : 'Internal Server Error'});
+        console.error('Auth error:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
 };
